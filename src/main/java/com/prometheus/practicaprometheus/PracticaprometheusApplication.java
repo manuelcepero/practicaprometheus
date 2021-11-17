cbb1fd398a71520c2821c0c5f31eb7c4c6c2f1d3
Manuel Cepero Sánchez <manuel.cepero@hotmail.com>
2021-11-17 09:51:12 +0100

    Subida del proyecto

---
+++ src/main/java/com/prometheus/practicaprometheus/PracticaprometheusApplication.java
@@ -0,0 +1,13 @@
+package com.prometheus.practicaprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaprometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaprometheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prometheus/practicaprometheus/PracticaprometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometheus.practicaprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaprometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
